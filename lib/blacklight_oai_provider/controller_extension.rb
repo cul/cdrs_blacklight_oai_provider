44889663cf4da5df8eb8e49b61ed5478f1fe80f8
JackBlackLight <jd2148@columbia.edu>
2014-10-15 14:00:28 -0400

    fixed resumption token dates

--- lib/blacklight_oai_provider/controller_extension.rb
+++ lib/blacklight_oai_provider/controller_extension.rb
@@ -31,6 +31,22 @@ module BlacklightOaiProvider::ControllerExtension
   end
 
   def add_date_range_param(solr_parameters, user_parameters)
+      token_string = user_parameters[:resumptionToken]
+      if(token_string)
+	matches = /(.+):(\d+)$/.match(token_string)
+	parts = matches.captures[0].split('.')
+	parts.each do |part|
+          case part
+          when /^f/
+	       partf = part.sub(/^f\(/, '').sub(/\)$/, '')
+	       user_parameters[:from] = partf
+          when /^u/
+	       partu = part.sub(/^u\(/, '').sub(/\)$/, '')
+	       user_parameters[:until] = partu
+          end
+        end
+      end
+
 
       if(user_parameters[:from].nil?)
 	    user_parameters[:from] = "0001-01-01T00:00:00Z"
